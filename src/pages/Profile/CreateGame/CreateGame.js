17334b8266efff8b0b5dead1886a5f8d99a6733f
datphits <datfit@Macintoshs-M1.local>
2022-04-05 18:34:20 +0700

    implement API

--- src/pages/Profile/CreateGame/CreateGame.js
+++ src/pages/Profile/CreateGame/CreateGame.js
@@ -17,10 +17,25 @@ import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
+import { create } from "ipfs-http-client";
+import { useWeb3React } from "@web3-react/core";
+import "react-toastify/dist/ReactToastify.css";
+import { read, createNetworkOrSwitch, write } from "src/services/web3";
+import { BSC_CHAIN_ID } from "src/consts/blockchain";
+import { RELAY_ADDRESS, FACTORY_ADDRESS, KAWAIIVERSE_NFT1155_ADDRESS } from "src/consts/address";
+import { URL } from "src/consts/constant";
+import FACTORY_ABI from "src/utils/abi/factory.json";
+import NFT1155_ABI from "src/utils/abi/KawaiiverseNFT1155.json";
+import RELAY_ABI from "src/utils/abi/relay.json";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+import axios from "axios";
+import { Spin } from "antd";
 
 const cx = cn.bind(styles);
 const web3 = new Web3(BSC_rpcUrls);
 const KAWAII1155_ADDRESS = "0xD6eb653866F629e372151f6b5a12762D16E192f5";
+const client = create("https://ipfs.infura.io:5001/api/v0");
 
 const CreateGame = () => {
     const [open, setOpen] = useState(false);
@@ -31,17 +46,50 @@ const CreateGame = () => {
     const [errorImage, setErrorImage] = useState(false);
     const [fileName, setFileName] = useState();
     const [gameSelected, setGameSelected] = useState(KAWAII1155_ADDRESS);
+    const [gameList, setGameList] = useState([]);
+    const [loadingGameList, setLoadingGameList] = useState(false);
+    const [uploadImageLoading, setUploadImageLoading] = useState(false);
+    const [uploadGameLoading, setUploadGameLoading] = useState(false);
+    const { account, chainId, library } = useWeb3React();
+
+    useEffect(() => {
+        logInfo();
+    }, [account]);
 
     const handleOpen = () => setOpen(true);
     const handleClose = () => {
         setOpen(false);
         setSuccess(false);
-        setErrorName(false);
-        setErrorSymbol(false);
-        setErrorImage(false);
-        setFileName();
+        // setErrorName(false);
+        // setErrorSymbol(false);
+        // setErrorImage(false);
+        // setFileName();
     };
     const history = useHistory();
+    const skeletonArray = Array.from(Array(8).keys());
+
+    const logInfo = async () => {
+        if (!account) return;
+        setGameList([]);
+        setLoadingGameList(true);
+        try {
+            let lists = [];
+            const totalGame = await read("nftOfUserLength", BSC_CHAIN_ID, FACTORY_ADDRESS, FACTORY_ABI, [account]);
+            for (let index = 0; index < totalGame; index++) {
+                let gameAddress = await read("nftOfUser", BSC_CHAIN_ID, FACTORY_ADDRESS, FACTORY_ABI, [account, index]);
+                let gameName = await read("name", BSC_CHAIN_ID, gameAddress, NFT1155_ABI, []);
+                const res = await axios.get(`${URL}/v1/game/logo?contract=${gameAddress}`);
+                let gameUrl = res.data.data[0] ? res.data.data[0].logoUrl : "";
+                lists.push({ gameAddress, gameName, gameUrl });
+            }
+            console.log(lists);
+            setGameList(lists);
+        } catch (err) {
+            console.log(err);
+        } finally {
+            setLoadingGameList(false);
+        }
+    };
 
     const inputChangeHandler = (key, value) => {
         setgameInfo({ ...gameInfo, [key]: value });
@@ -57,15 +105,111 @@ const CreateGame = () => {
         inputChangeHandler("symbol", e.target.value);
     };
 
-    const handleUploadImage = e => {
+    const handleUploadImage = async e => {
+        setUploadImageLoading(true);
         e.target.files[0] ? setErrorImage(false) : setErrorImage(true);
+
+        if (!e.target.files[0]) return;
+
+        const file = e.target.files[0];
         setFileName(e.target.files[0].name);
-        inputChangeHandler("avatar", e.target.files[0]);
+        try {
+            const added = await client.add(file);
+            const url = `https://ipfs.infura.io/ipfs/${added.path}`;
+            inputChangeHandler("avatar", url);
+            console.log(url);
+        } catch (error) {
+            console.log("Error uploading file: ", error);
+        } finally {
+            setUploadImageLoading(false);
+        }
     };
 
-    const handleCreate = () => {
+    const handleCreate = async () => {
+        setUploadGameLoading(true);
         console.log(checkValidation());
-        checkValidation() == 1 ? setSuccess(true) : setSuccess(false);
+        // checkValidation() == 1 ? setSuccess(true) : setSuccess(false);
+        if (checkValidation()) {
+            const _data = web3.eth.abi.encodeFunctionCall(
+                {
+                    inputs: [
+                        {
+                            internalType: "address",
+                            name: "_owner",
+                            type: "address",
+                        },
+                        {
+                            internalType: "address",
+                            name: "_imp",
+                            type: "address",
+                        },
+                        {
+                            internalType: "string",
+                            name: "_name",
+                            type: "string",
+                        },
+                        {
+                            internalType: "string",
+                            name: "_symbol",
+                            type: "string",
+                        },
+                    ],
+                    name: "createNFT1155",
+                    outputs: [],
+                    stateMutability: "nonpayable",
+                    type: "function",
+                },
+                [account, KAWAIIVERSE_NFT1155_ADDRESS, gameInfo.name, gameInfo.symbol],
+            );
+
+            try {
+                if (chainId !== BSC_CHAIN_ID) {
+                    const error = await createNetworkOrSwitch(library.provider);
+                    if (error) {
+                        throw new Error("Please change network to Testnet Binance smart chain.");
+                    }
+                }
+                await write(
+                    "execute",
+                    library.provider,
+                    RELAY_ADDRESS,
+                    RELAY_ABI,
+                    [FACTORY_ADDRESS, _data],
+                    { from: account },
+                    hash => {
+                        console.log(hash);
+                    },
+                );
+                logInfo();
+
+                const totalGame = await read("nftOfUserLength", BSC_CHAIN_ID, FACTORY_ADDRESS, FACTORY_ABI, [account]);
+                let gameAddress = await read("nftOfUser", BSC_CHAIN_ID, FACTORY_ADDRESS, FACTORY_ABI, [
+                    account,
+                    totalGame - 1,
+                ]);
+
+                let bodyParams = {
+                    contract: gameAddress,
+                    logoUrl: gameInfo.avatar,
+                    sign: "signature",
+                    // data: listNft,
+                };
+                console.log(bodyParams);
+
+                const res = await axios.post(`${URL}/v1/game/logo`, bodyParams);
+                if (res.status === 200) {
+                    console.log(res);
+                }
+                setSuccess(true);
+            } catch (err) {
+                setSuccess(false);
+                console.log(err.response);
+            } finally {
+                setUploadGameLoading(false);
+            }
+        } else {
+            setUploadGameLoading(false);
+        }
     };
 
     const checkValidation = () => {
@@ -92,118 +236,57 @@ const CreateGame = () => {
                             </CardContent>
                         </Card>
                     </Grid>
-                    <Grid item md={4} sm={6} xs={12}>
-                        <Card className={cx("item-card", "card")}>
-                            <CardContent>
-                                <Typography className={cx("item-header")}>
-                                    <img src={logoKawaii} alt="logo" className={cx("game-logo")} />
-                                    Kawaii Islands
-                                </Typography>
-                                <Typography className={cx("item-paragraph")}>
-                                    <img src={logoLayers} alt="logo" className={cx("game-mini")} />
-                                    Items: <span className={cx("game-amount")}>100</span>
-                                </Typography>
-                                <Typography className={cx("item-paragraph")}>
-                                    <img src={logoTrend} alt="logo" className={cx("game-mini")} />
-                                    Total sale: <span className={cx("game-amount")}>1,000,000 KWT</span>
-                                </Typography>
-                            </CardContent>
-                            <CardActions className={cx("create-action")}>
-                                <Button
-                                    size="small"
-                                    className={cx("create-button")}
-                                    onClick={() => history.push(`profile/manage-nft/${gameSelected}`)}
-                                >
-                                    Join now
-                                </Button>
-                            </CardActions>
-                        </Card>
-                    </Grid>
-                    <Grid item md={4} sm={6} xs={12}>
-                        <Card className={cx("item-card", "card")}>
-                            <CardContent>
-                                <Typography className={cx("item-header")}>
-                                    <img src={logoKawaii} alt="logo" className={cx("game-logo")} />
-                                    Kawaii Islands
-                                </Typography>
-                                <Typography className={cx("item-paragraph")}>
-                                    <img src={logoLayers} alt="logo" className={cx("game-mini")} />
-                                    Items: <span className={cx("game-amount")}>100</span>
-                                </Typography>
-                                <Typography className={cx("item-paragraph")}>
-                                    <img src={logoTrend} alt="logo" className={cx("game-mini")} />
-                                    Total sale: <span className={cx("game-amount")}>1,000,000 KWT</span>
-                                </Typography>
-                            </CardContent>
-                            <CardActions className={cx("create-action")}>
-                                <Button
-                                    size="small"
-                                    className={cx("create-button")}
-                                    onClick={() => history.push("profile/manage-nft")}
-                                >
-                                    Join now
-                                </Button>
-                            </CardActions>
-                        </Card>
-                    </Grid>
-                    <Grid item md={4} sm={6} xs={12}>
-                        <Card className={cx("item-card", "card")}>
-                            <CardContent>
-                                <Typography className={cx("item-header")}>
-                                    <img src={logoKawaii} alt="logo" className={cx("game-logo")} />
-                                    Kawaii Islands
-                                </Typography>
-                                <Typography className={cx("item-paragraph")}>
-                                    <img src={logoLayers} alt="logo" className={cx("game-mini")} />
-                                    Items: <span className={cx("game-amount")}>100</span>
-                                </Typography>
-                                <Typography className={cx("item-paragraph")}>
-                                    <img src={logoTrend} alt="logo" className={cx("game-mini")} />
-                                    Total sale: <span className={cx("game-amount")}>1,000,000 KWT</span>
-                                </Typography>
-                            </CardContent>
-                            <CardActions className={cx("create-action")}>
-                                <Button
-                                    size="small"
-                                    className={cx("create-button")}
-                                    onClick={() => history.push("profile/manage-nft")}
-                                >
-                                    Join now
-                                </Button>
-                            </CardActions>
-                        </Card>
-                    </Grid>
-                    <Grid item md={4} sm={6} xs={12}>
-                        <Card className={cx("item-card", "card")}>
-                            <CardContent>
-                                <Typography className={cx("item-header")}>
-                                    <img src={logoKawaii} alt="logo" className={cx("game-logo")} />
-                                    Kawaii Islands
-                                </Typography>
-                                <Typography className={cx("item-paragraph")}>
-                                    <img src={logoLayers} alt="logo" className={cx("game-mini")} />
-                                    Items: <span className={cx("game-amount")}>100</span>
-                                </Typography>
-                                <Typography className={cx("item-paragraph")}>
-                                    <img src={logoTrend} alt="logo" className={cx("game-mini")} />
-                                    Total sale: <span className={cx("game-amount")}>1,000,000 KWT</span>
-                                </Typography>
-                            </CardContent>
-                            <CardActions className={cx("create-action")}>
-                                <Button
-                                    size="small"
-                                    className={cx("create-button")}
-                                    onClick={() => history.push("profile/manage-nft")}
-                                >
-                                    Join now
-                                </Button>
-                            </CardActions>
-                        </Card>
-                    </Grid>
+
+                    {!loadingGameList ? (
+                        gameList.map((item, idx) => (
+                            <Grid item md={4} sm={6} xs={12} key={idx}>
+                                <Card className={cx("item-card", "card")}>
+                                    <CardContent>
+                                        <Typography className={cx("item-header")}>
+                                            <img
+                                                src={item.gameUrl != "" ? item.gameUrl : logoKawaii}
+                                                alt="logo"
+                                                className={cx("game-logo")}
+                                            />
+                                            {/* Kawaii Islands */}
+                                            {item.gameName}
+                                        </Typography>
+                                        <Typography className={cx("item-paragraph")}>
+                                            <img src={logoLayers} alt="logo" className={cx("game-mini")} />
+                                            Items: <span className={cx("game-amount")}>100</span>
+                                        </Typography>
+                                        <Typography className={cx("item-paragraph")}>
+                                            <img src={logoTrend} alt="logo" className={cx("game-mini")} />
+                                            Total sale: <span className={cx("game-amount")}>1,000,000 KWT</span>
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions className={cx("create-action")}>
+                                        <Button
+                                            size="small"
+                                            className={cx("create-button")}
+                                            onClick={() => history.push(`profile/manage-nft/${gameSelected}`)}
+                                        >
+                                            Join now
+                                        </Button>
+                                    </CardActions>
+                                </Card>
+                            </Grid>
+                        ))
+                    ) : (
+                        <>
+                            {skeletonArray.map((item, idx) => (
+                                <Grid item md={4} sm={6} xs={12} key={idx}>
+                                    <SkeletonTheme baseColor="#3D1C6C" highlightColor="#402A7D" duration={2}>
+                                        <Skeleton className={cx("skeleton")} />
+                                    </SkeletonTheme>
+                                </Grid>
+                            ))}
+                        </>
+                    )}
                 </Grid>
                 <Modal open={open} onClose={handleClose}>
                     <div className={cx("modal-style")}>
-                        {console.log(success)}
+                        {/* {console.log(success)} */}
                         {success == false ? (
                             <>
                                 <Typography className={cx("modal_header")}>CREATE GAME</Typography>
@@ -211,6 +294,7 @@ const CreateGame = () => {
                                     placeholder="Name"
                                     className={errorName == false ? cx("input") : cx("input_error")}
                                     required
+                                    value={gameInfo.name}
                                     onChange={handleChangeName}
                                 />
                                 {errorName == true ? (
@@ -261,9 +345,15 @@ const CreateGame = () => {
                                     )}
                                 </div>
 
-                                <Button className={cx("modal_create")} onClick={handleCreate}>
-                                    Create now
-                                </Button>
+                                {!uploadImageLoading && !uploadGameLoading ? (
+                                    <Button className={cx("modal_create")} onClick={handleCreate}>
+                                        Create now
+                                    </Button>
+                                ) : (
+                                    <Button className={cx("modal_create")}>
+                                        <Spin />
+                                    </Button>
+                                )}
                             </>
                         ) : (
                             <div className={cx("modal_success")}>
