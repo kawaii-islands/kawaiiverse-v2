e2aee3005ce740dc67a8c26d093c962d8d3178b2
duong-orai <duong.nt@orai.io>
2022-04-21 10:38:25 +0700

    evmos env

--- src/components/ConnectWalletModal/index.js
+++ src/components/ConnectWalletModal/index.js
@@ -11,115 +11,116 @@ import { Typography, Button, SvgIcon } from "@mui/material";
 import { ReactComponent as LogoutIcon } from "src/assets/icons/log-out.svg";
 
 const style = {
-  position: "absolute",
-  top: 48,
-  right: 0,
-  background: "#ffffff",
-  color: "#3e3636",
-  height: "calc(100vh - 48px)",
-  padding: "30px 25px",
-  maxWidth: 340,
+    position: "absolute",
+    top: 48,
+    right: 0,
+    background: "#ffffff",
+    color: "#3e3636",
+    height: "calc(100vh - 48px)",
+    padding: "30px 25px",
+    maxWidth: 340,
 };
 
 const buttonStyle = {
-  width: "100%",
-  marginTop: 15,
-  height: 58,
-  justifyContent: "left",
+    width: "100%",
+    marginTop: 15,
+    height: 58,
+    justifyContent: "left",
 };
 
 const logoutStyle = {
-  background: "transparent",
-  position: "absolute",
-  bottom: 10,
-  left: 0,
-  width: "100%",
-  borderRadius: 0,
-  paddingLeft: 2,
-  justifyContent: "left",
-  paddingTop: 2,
-  borderTop: "1px solid #1976d2",
-  "&:hover": {
     background: "transparent",
-  },
+    position: "absolute",
+    bottom: 10,
+    left: 0,
+    width: "100%",
+    borderRadius: 0,
+    paddingLeft: 2,
+    justifyContent: "left",
+    paddingTop: 2,
+    borderTop: "1px solid #1976d2",
+    "&:hover": {
+        background: "transparent",
+    },
 };
 
 const MetaMask = {
-  name: "Metamask",
-  icon: metamask,
-  id: "MetaMask",
-  connector: InjectedConnector,
-  connectorName: "MetaMask",
+    name: "Metamask",
+    icon: metamask,
+    id: "MetaMask",
+    connector: InjectedConnector,
+    connectorName: "MetaMask",
 };
 
 const WalletConnect = {
-  name: "Wallet Connect",
-  icon: walletconnectIcon,
-  id: "WalletConnect",
-  connector: WalletConnectConnector,
-  connectorName: "WalletConnect",
+    name: "Wallet Connect",
+    icon: walletconnectIcon,
+    id: "WalletConnect",
+    connector: WalletConnectConnector,
+    connectorName: "WalletConnect",
 };
 
 const CONNECTORS = [MetaMask, WalletConnect];
 
 const ListConnector = ({ connectWallet }) => {
-  return CONNECTORS.map((connector, idx) => (
-    <Button
-      key={idx}
-      onClick={() => connectWallet(connector.connectorName, idx)}
-      variant="outlined"
-      style={buttonStyle}
-    >
-      <img src={connector.icon} alt="" />
-      <Typography sx={{ ml: 2 }}>{connector.name}</Typography>
-    </Button>
-  ));
+    return CONNECTORS.map((connector, idx) => (
+        <Button
+            key={idx}
+            onClick={() => connectWallet(connector.connectorName, idx)}
+            variant="outlined"
+            style={buttonStyle}
+        >
+            <img src={connector.icon} alt="" />
+            <Typography sx={{ ml: 2 }}>{connector.name}</Typography>
+        </Button>
+    ));
 };
 
 const ConnectWalletModal = ({ show, setShow, alwaysShow }) => {
-  const context = useWeb3React();
-  const { connector, activate, account, deactivate } = context;
-  const [activatingConnector, setActivatingConnector] = useState();
-  useEffect(() => {
-    if (activatingConnector && activatingConnector === connector) {
-      setActivatingConnector(undefined);
-    }
-  }, [activatingConnector, connector]);
+    const context = useWeb3React();
+    const { connector, activate, account, deactivate } = context;
+    const [activatingConnector, setActivatingConnector] = useState();
+    useEffect(() => {
+        if (activatingConnector && activatingConnector === connector) {
+            setActivatingConnector(undefined);
+        }
+    }, [activatingConnector, connector]);
 
-  const connectWallet = async name => {
-    const currentConnector = connectorsByName[name];
-    setActivatingConnector(currentConnector);
-    await activate(connectorsByName[name]);
-    setShow(false);
-  };
-
-  return (
-    <Modal
-      open={show}
-      onClose={() => {
-        if (alwaysShow) return;
+    const connectWallet = async name => {
+        const currentConnector = connectorsByName[name];
+        setActivatingConnector(currentConnector);
+        console.log(currentConnector);
+        await activate(connectorsByName[name]);
         setShow(false);
-      }}
-    >
-      <Box sx={style}>
-        <Typography variant="h5">Connect wallet</Typography>
-        <Typography variant="subtitle2" sx={{ mt: 2, mb: 1 }}>
-          Connect with one of available wallet providers or create a new wallet.
-        </Typography>
-        <ListConnector connectWallet={connectWallet} />
-        {account && (
-          <Button onClick={deactivate} sx={logoutStyle}>
-            <SvgIcon>
-              <LogoutIcon fill="#1976d2" />
-            </SvgIcon>
-            <Typography variant="subtitle1" sx={{ ml: 1 }}>
-              Log out
-            </Typography>
-          </Button>
-        )}
-      </Box>
-    </Modal>
-  );
+    };
+
+    return (
+        <Modal
+            open={show}
+            onClose={() => {
+                if (alwaysShow) return;
+                setShow(false);
+            }}
+        >
+            <Box sx={style}>
+                <Typography variant="h5">Connect wallet</Typography>
+                <Typography variant="subtitle2" sx={{ mt: 2, mb: 1 }}>
+                    Connect with one of available wallet providers or create a new wallet.
+                </Typography>
+                <ListConnector connectWallet={connectWallet} />
+                {account && (
+                    <Button onClick={deactivate} sx={logoutStyle}>
+                        <SvgIcon>
+                            <LogoutIcon fill="#1976d2" />
+                        </SvgIcon>
+                        <Typography variant="subtitle1" sx={{ ml: 1 }}>
+                            Log out
+                        </Typography>
+                    </Button>
+                )}
+            </Box>
+        </Modal>
+    );
 };
 
 export default ConnectWalletModal;
